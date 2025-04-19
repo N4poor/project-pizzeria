ec18acdb6d93f6d4477836995d8191dfca856a42
N4poor <braveeesoul@gmail.com>
2025-04-19 15:20:07 +0200

    added submodule 10.3 changes

--- dist/js/components/booking.js
+++ dist/js/components/booking.js
@@ -15,6 +15,50 @@ class Booking {
       thisBooking.getData();
     }
 
+    sendBooking(){
+      const thisBooking = this;
+
+      const url = settings.db.url + '/' + settings.db.bookings;
+
+      const hourStr = utils.numberToHour(thisBooking.hour); // "12:30"
+
+      const payLoad = {
+        date: thisBooking.date,
+        hour: hourStr,
+        table: thisBooking.selectedTable !==null ? parseInt(thisBooking.selectedTable) : null,
+        duration: parseInt(thisBooking.hoursAmount.value),
+        ppl: parseInt(thisBooking.peopleAmount.value),
+        starters: [],
+        phone: thisBooking.dom.wrapper.querySelector('input[name="phone"]').value,
+        address: thisBooking.dom.wrapper.querySelector('input[name="address"]').value,
+      };
+
+      const starterInputs = thisBooking.dom.wrapper.querySelectorAll('input[name="starter"]:checked');
+      for (let input of starterInputs){
+        payLoad.starters.push(input.value);
+      }
+
+      const options = {
+        method: "POST",
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(payLoad),
+      };
+
+      fetch(url,options)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        console.log('booking confirmed:', parsedResponse);
+        
+        thisBooking.makeBooked(payLoad.date, hourStr, payLoad.duration, payLoad.table);
+
+        thisBooking.updateDOM();
+
+        alert('Rezerwacja potwierdzona!');
+      });
+    }
+
     initTables(){
       const thisBooking = this;
 
@@ -238,6 +282,16 @@ class Booking {
       thisBooking.updateDOM();
     });
     
+    thisBooking.dom.wrapper.querySelector('form.booking-form').addEventListener('submit', function(event){
+      event.preventDefault();
+
+      if (thisBooking.selectedTable === null) {
+        alert('Wybierz stolik przed wysłaniem rezerwacji.');
+        return;
+      }
+
+      thisBooking.sendBooking();
+    });
   }
   }
   
